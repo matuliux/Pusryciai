78e0ca8fe82f086e492dc1d307854adecee9259f
matuliux <100164021+matuliux@users.noreply.github.com>
2022-04-22 09:21:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,11 @@ namespace Matas_Lukšys_darbo_užduotis
             Kiausinis kiausinis = Kepimas(kiausiniu_kiekis);
             Sonine sonine = Kepimas(kiausiniu_kiekis, sonines_gabaliuku_kiekis);    // Pateikiame 2 kintamuosiu, siekiant suzinoti ar reikia sildyti keptuve
             Skrebutis skrebutis = Skrebucio_skrudinimas();
-
+            
+            
+            //Klasems kiausinis, sonine, skrebutis sukuriau motinine klase Maistas ir joje aprasiau metoda Dejimas, nes visom trim klasem reikia atlikti ta pati veiksma
+            //Dejimas parodo, kas dedama i lekste 
+            
             kiausinis.Dejimas();
             sonine.Dejimas();
             skrebutis.Dejimas();
@@ -40,13 +44,13 @@ namespace Matas_Lukšys_darbo_užduotis
             
         }
 
-        public static int Kiausiniu_kiekio_nustymas()
+        public static int Kiausiniu_kiekio_nustymas()                   // Funkcija, suzinanti kiausiniu kieki
         {
             int kiausiniu_kiekis, temp; int patikrinimas = 0;
             string kiausiniu_kiekis_string = "";
             Console.WriteLine("Kiek kiausiniu kepsime?");
 
-            while (patikrinimas == 0)
+            while (patikrinimas == 0)                                   // Tikriname ar ivedimas yra skaicius, jei ne, parasome klaida ir prasome ivesti dar karta
             {
                 kiausiniu_kiekis_string = Console.ReadLine();
                 
@@ -65,13 +69,13 @@ namespace Matas_Lukšys_darbo_užduotis
 
         }
 
-        public static int Sonines_kiekio_nustymas()
+        public static int Sonines_kiekio_nustymas()                         // Sonines kiekio nustatymas
         {
             int sonines_gabaliuku_kiekis, temp; int patikrinimas = 0;
             string sonines_gabaliuku_kiekis_string = "";
             Console.WriteLine("Kiek sonines gabaliuku kepsime?");
 
-            while (patikrinimas == 0) {
+            while (patikrinimas == 0) {                                     // Tikriname ar ivedimas yra skaicius, jei ne, parasome klaida ir prasome ivesti dar karta
 
                 sonines_gabaliuku_kiekis_string = Console.ReadLine();
 
@@ -90,7 +94,7 @@ namespace Matas_Lukšys_darbo_užduotis
 
         }
 
-        public static Pasiruosimas Pasiruosimas_darbui()
+        public static Pasiruosimas Pasiruosimas_darbui()            // Atspausdiname pasiruosimo zingsnius
         {
             Console.WriteLine("\nParuosiami indai");
             Thread.Sleep(500);
@@ -101,14 +105,14 @@ namespace Matas_Lukšys_darbo_užduotis
 
         }
 
-        public static Kiausinis Kepimas(int kiek)
+        public static Kiausinis Kepimas(int kiek)                   // Atspausdiname kiausiniu kepimo zingsnius
         {
-            if (kiek > 0)
+            if (kiek > 0)                                           // Jeigu turime kiausiniu, sildome keptuve ir juos kepame
             {
                 Console.WriteLine("I keptuve ipilamas ir sildomas aliejus\n");
                 Thread.Sleep(500);
 
-                for (int i = 0; i < kiek; i++)
+                for (int i = 0; i < kiek; i++)                      // Kepame visus kiausinius
                 {
                     Console.WriteLine("Kepamas {0}-(as) kiausinis", i + 1);
                     Thread.Sleep(500);
@@ -116,7 +120,7 @@ namespace Matas_Lukšys_darbo_užduotis
                 Console.WriteLine("Kiausianiai paruosti!\n");
             }
 
-            else
+            else                                                    // Jei kiausiniu neturime ju nekepame
             {
                 Console.WriteLine("Kiausiniu nekepame, nes ju neturime\n");
             }
@@ -124,17 +128,18 @@ namespace Matas_Lukšys_darbo_užduotis
             return new Kiausinis();
         }
 
-        public static Sonine Kepimas(int kiek_k, int kiek_s) //funkcijoje kiausiniu kieki vadinsime kiek_k, sonines kieki vadinsime kiek_s
+        public static Sonine Kepimas(int kiek_k, int kiek_s)        // Funkcijoje kiausiniu kieki vadinsime kiek_k, sonines kieki vadinsime kiek_s
+                                                                    // Atspausdiname sonines kepimo zingsnius
         {
-            if(kiek_k == 0 && kiek_s > 0)
+            if(kiek_k == 0 && kiek_s > 0)                           // Tikriname ar reikia sildyti alieju
             {
                 Console.WriteLine("I keptuve ipilamas ir sildomas aliejus\n");
                 Thread.Sleep(500);
             }
 
-            if(kiek_s > 0)
+            if(kiek_s > 0)                                          // Jei turime sonines, ja kepsime
             {
-                for (int i = 0; i < kiek_s; i++)
+                for (int i = 0; i < kiek_s; i++)                    // Kepame visa sonine
                 {
                     Console.WriteLine("Kepamas {0}-(as) sonines gabaliukas", i + 1);
                     Thread.Sleep(500);
@@ -143,7 +148,7 @@ namespace Matas_Lukšys_darbo_užduotis
 
             }
 
-            else if(kiek_s == 0)
+            else if(kiek_s == 0)                                    // Jei sonines neturime, jos nekepame
             {
                 Console.WriteLine("Sonines nekepame, nes jos neturime\n");
             }
@@ -151,7 +156,7 @@ namespace Matas_Lukšys_darbo_užduotis
             return new Sonine();
         }
 
-        public static Skrebutis Skrebucio_skrudinimas()
+        public static Skrebutis Skrebucio_skrudinimas()                     // Atspausdiname skrebucio skurdinima
         {
             Console.WriteLine("Idedame skrebuti i skrudintuve");
             Thread.Sleep(500);
@@ -162,7 +167,7 @@ namespace Matas_Lukšys_darbo_užduotis
             return new Skrebutis();
         }
 
-        public static Kava Kavos_virimas()
+        public static Kava Kavos_virimas()                                  // Atspausdiname kavos virima
         {
             Console.WriteLine("\nUzkaiciame vandeni");
             Thread.Sleep(500);
